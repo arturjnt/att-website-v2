9340c5d2071a92312fba09ed634e0a8f52176d55
Artur Teixeira <arturjnt@gmail.com>
2021-12-14 14:47:50 +0000

    Update astro, tailwindcc and vue renderer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
+// tailwind.config.cjs
 module.exports = {
-    mode: 'jit',
-    purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
-    theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            black: '#000000',
-            white: '#FFFFFF',
-            orange: { light: '#EEC170', DEFAULT: '#F2A65A', dark: '#F58549' },
-            blue: { light: 'rgba(70, 78, 95, 0.05)', DEFAULT: '#464E5F', dark: '#2F3644', fields: 'rgb(245, 246, 247)' },
-            grey: { DEFAULT: '#C4C4C4' }
-        }
-    }
+  mode: "jit",
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000000",
+      white: "#FFFFFF",
+      orange: { light: "#EEC170", DEFAULT: "#F2A65A", dark: "#F58549" },
+      blue: {
+        light: "rgba(70, 78, 95, 0.05)",
+        DEFAULT: "#464E5F",
+        dark: "#2F3644",
+        fields: "rgb(245, 246, 247)",
+      },
+      grey: { DEFAULT: "#C4C4C4" },
+    },
+  },
 };
